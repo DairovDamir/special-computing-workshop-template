7cf5cfff3db7e89544f33406c78b788789f97735
DamirDairov <dairov3050@gmail.com>
2024-03-17 14:54:37 +0300

    Task3

---
+++ src/main/java/ru/spbu/apcyb/svp/tasks/task4/FileWriter.java
@@ -0,0 +1,96 @@
+package ru.spbu.apcyb.svp.tasks.task4;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
+
+public class FileWriter {
+
+    private final double[] arguments;
+    private final List<Double> tanInMultiThreading = Collections.synchronizedList(new ArrayList<>());
+
+    private final List<Double> tanInSingleThreading = new ArrayList<>();
+
+    public FileWriter(Path path, int numOfArguments) throws IOException{
+        arguments = new double[numOfArguments];
+        try(BufferedWriter bufferedWriter = Files.newBufferedWriter(path)) {
+            for (int i = 0; i < numOfArguments; i++) {
+                arguments[i] = Math.random();
+                bufferedWriter.write(String.valueOf(arguments[i]));
+                bufferedWriter.newLine();
+            }
+        } catch (IOException e) {throw new NoSuchFileException("File does not exist");}
+    }
+
+    public double getArguments(int i) {
+        return arguments[i];
+    }
+
+    public int getNumbersOfArguments() {
+        return arguments.length;
+    }
+
+    public  void setTanInMultiThreading(double num) {
+        tanInMultiThreading.add(num);
+    }
+
+    public void writeTanMultiThreadingValues(Path path1) {
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path1)) {
+            for (int i = 0; i < tanInMultiThreading.size(); i++) {
+                bufferedWriter.write(String.valueOf(tanInMultiThreading.get(i)));
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.write("count = " + tanInMultiThreading.size());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void writeTanSingleThreadedValues(Path path1) {
+        for (int i = 0; i < arguments.length; i++) {
+            tanInSingleThreading.add(Math.tan(arguments[i]));
+        }
+
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path1)) {
+            for (int i = 0; i < tanInSingleThreading.size(); i++) {
+                bufferedWriter.write(String.valueOf(tanInSingleThreading.get(i)));
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.write("count = " + tanInSingleThreading.size());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void calculate(int num, FileWriter fileWriter) throws IOException, InterruptedException {
+        Thread thread = null;
+            if (num == 0) {
+                throw new IOException("Number of arguments is zero");
+            } else if (num < 10) {
+                for (int i = 0; i < num; i++) {
+                     thread = new Thread(new Multithreading(i, i, fileWriter));
+                     thread.start();
+                }
+            } else {
+                int remain = num % 10;
+                if (remain == 0) {
+                    for (int i = 0; i < 10; i++) {
+                         thread = new Thread(new Multithreading(i * num / 10,
+                                 (i * num / 10) + (num / 10) - 1, fileWriter));
+                         thread.start();
+                    }
+                } else {
+                    for (int i = 0; i < 10; i++) {
+                        if (i < 9){
+                            thread = new Thread(new Multithreading(i * (1 + (num / 10)),
+                                (i * (1 + (num / 10))) + (1 + (num / 10)) - 1, fileWriter));
+                        } else {thread = new Thread(new Multithreading(9 * (1 + (num / 10)),
+                                num - 1, fileWriter));}
+                        thread.start();
+                    }
+               }
+            }
+            thread.join();
+    }
+}
---
+++ src/main/java/ru/spbu/apcyb/svp/tasks/task4/Multithreading.java
@@ -0,0 +1,52 @@
+package ru.spbu.apcyb.svp.tasks.task4;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Scanner;
+
+public class Multithreading implements Runnable{
+    private final int startNum;
+    private final int finishNum;
+    private final FileWriter fileWriter;
+
+    public Multithreading(int startNum, int finishNum, FileWriter fileWriter) {
+        this.startNum = startNum;
+        this.finishNum = finishNum;
+        this.fileWriter = fileWriter;
+    }
+    @Override
+    public void run() {
+            for (int i = startNum; i <= finishNum; i++) {
+                fileWriter.setTanInMultiThreading(Math.tan(fileWriter.getArguments(i)));
+                System.out.println(i);
+            }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException{
+        int num = 0;
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
+
+        Path path = Path.of(args[0]);
+        FileWriter fileWriter = new FileWriter(path, num);
+
+
+        double startOfWork = System.currentTimeMillis();
+        fileWriter.calculate(num, fileWriter);
+        fileWriter.writeTanMultiThreadingValues(Path.of(args[1]));
+        double finishOfWork = System.currentTimeMillis();
+
+        double timeOfWorkInMultiThreading = finishOfWork - startOfWork;
+
+        startOfWork = System.currentTimeMillis();
+        fileWriter.writeTanSingleThreadedValues(Path.of(args[1]));
+        finishOfWork = System.currentTimeMillis();
+
+        double timeOfWorkInSingleThreading = finishOfWork - startOfWork;
+
+        System.out.print("Time in Mult: " + timeOfWorkInMultiThreading +
+                "\n" + "Time in Single: " + timeOfWorkInSingleThreading);
+
+    }
+}
+
